93e8a0febed21198f14638b860425eba2e23d026
Tone <372375692@qq.com>
2018-07-04 11:44:23 +0800

    Initial commit

---
+++ bottomtabview/src/main/java/com/tone/bottomtabview/BottomTabView.java
@@ -0,0 +1,100 @@
+package com.tone.bottomtabview;
+
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class BottomTabView extends TabLayout {
+
+    private SparseArray<TabItem> tabItems = new SparseArray<>();
+    private LayoutInflater inflater;
+    private OnTabItemSelectedListener selectedListener;
+
+    public BottomTabView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BottomTabView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public BottomTabView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+
+    private void init(Context context) {
+
+        inflater = LayoutInflater.from(context);
+
+
+        addOnTabSelectedListener(new OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(Tab tab) {
+                if (tab.getCustomView() != null)
+                    tab.getCustomView().setSelected(true);
+                if (selectedListener != null)
+                    selectedListener.onTabSelected(tab,getSelectedTabPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(Tab tab) {
+                if (tab.getCustomView() != null)
+                    tab.getCustomView().setSelected(false);
+                if (selectedListener != null)
+                    selectedListener.onTabUnselected(tab,tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabReselected(Tab tab) {
+
+            }
+        });
+
+    }
+
+
+    public void setTabItems(SparseArray<TabItem> tabItems) {
+        this.tabItems = tabItems;
+        int count = getTabCount();
+        for (int i = 0; i < count; i++) {
+            TabItem item = tabItems.get(i);
+            Tab tab = getTabAt(i);
+
+            View view = inflater.inflate(R.layout.bottom_tab_view_item_layout, null);
+            ImageView imageView = view.findViewById(R.id.bottom_tab_view_item_icon);
+            TextView textView = view.findViewById(R.id.bottom_tab_view_item_text);
+            textView.setText(item.getTitle());
+            textView.setTextColor(getTabTextColors());
+            imageView.setImageResource(item.getResId());
+            imageView.setImageTintList(getTabTextColors());
+
+            tab.setCustomView(view);
+        }
+    }
+
+
+    public void setSelectedListener(OnTabItemSelectedListener selectedListener) {
+        this.selectedListener = selectedListener;
+    }
+
+    public interface OnTabItemSelectedListener {
+
+        public void onTabSelected(Tab tab,int position);
+
+        public void onTabUnselected(Tab tab,int position);
+
+    }
+
+
+}
---
+++ bottomtabview/src/main/java/com/tone/bottomtabview/TabItem.java
@@ -0,0 +1,28 @@
+package com.tone.bottomtabview;
+
+public class TabItem {
+
+    private String title;
+    private int resId;
+
+    public TabItem(String title, int resId) {
+        this.title = title;
+        this.resId = resId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getResId() {
+        return resId;
+    }
+
+    public void setResId(int resId) {
+        this.resId = resId;
+    }
+}
